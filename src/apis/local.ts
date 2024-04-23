80318730e84b5a1aebecff68251f6b162888994d
peetzweg/ <phil.czek@gmail.com>
2024-04-23 09:11:13 +0200

    adds a local and relay api

--- src/apis/local.ts
+++ src/apis/local.ts
@@ -1,6 +1,11 @@
+import "../interfaces/local/augment-api";
+import "../interfaces/local/augment-types";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 
-const wsProvider = new WsProvider(`ws://127.0.0.1:9944`);
+const RPC = Bun.env.LOCAL_RPC;
+console.log("RPC: ", RPC);
+
+const wsProvider = new WsProvider(`ws://${RPC}`);
 
 export const api = await ApiPromise.create({
   provider: wsProvider,
