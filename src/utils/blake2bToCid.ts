9ab12bd29ded046715e04cf14056b520a9ae0f42
peetzweg/ <phil.czek@gmail.com>
2024-04-24 12:30:50 +0200

    make sure to use the correct codec

--- src/utils/blake2bToCid.ts
+++ src/utils/blake2bToCid.ts
@@ -2,8 +2,8 @@ import CID from "cids";
 import Multihashes from "multihashes";
 import { hexToU8a } from "@polkadot/util";
 
-export const blake2bToCid = (hexHash: string): CID => {
+export const blake2bToCid = (hexHash: string, codec = "json"): CID => {
   const byteHash = hexToU8a(hexHash);
   const multihash = Multihashes.encode(byteHash, "blake2b-256");
-  return new CID(1, "blake2b-256", multihash);
+  return new CID(1, codec, multihash);
 };
