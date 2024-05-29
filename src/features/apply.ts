88dce293d7ce357f021a08f6d0750531aab968be
peetzweg/ <phil.czek@gmail.com>
2024-05-29 14:13:45 +0200

    submit random already existing evidence from ipfs

--- src/features/apply.ts
+++ src/features/apply.ts
@@ -6,6 +6,15 @@ import { mnemonicGenerate } from "@polkadot/util-crypto";
 import { resolveOnInBlock } from "../utils/resolveOn";
 import { resolveOnSome } from "../utils/resolveOnSome";
 
+const UPLOADED_EVIDENCE_HASHES = [
+  "0x039cf2c852d0458552232b335caceb7303ea258869cc684b1ea14c0a927672ef",
+  "0xe81830c7d3b35e34354d0805498657ef06efa0980bb1e4a9b66197dfdd0883c9",
+  "0xfcad2b754ac4f78b6e808e4afdcd8300a073995a862a8ee21bd737189dec60c0",
+  "0x78597cf7170d9330c3326b9efba95a7f4f2d91e9d48f7211f7e2fbbf0a4970dd",
+  "0x2c1a85a79750622bcb4ad38ebb993ea792fdd310e765a65975b03a327cbd9441",
+  "0xf39c293d7f138130409bd9aaed2d8b1c41b792154052a1d086161561c491c3ea",
+];
+
 export const apply = async (amount: number) => {
   const People = await getApi("People");
   const Bulletin = await getApi("Bulletin");
@@ -34,10 +43,10 @@ export const apply = async (amount: number) => {
     // ProceduralPersonal: 'u16',
     // Procedural: '(u16,u8)',
     // ProceduralDerivative: '(u64,Option<u64>)'
-    await commit([{ ProceduralAccount: 5 }, null], applicant);
+    await commit([{ ProceduralAccount: 8 }, null], applicant);
     console.log(applicant.address, "committed");
 
-    console.log("waiting for authorization...");
+    // console.log("waiting for authorization...");
     // await authorizations([
     //   {
     //     Account: Bulletin.createType("AccountId", applicant.address),
@@ -45,10 +54,12 @@ export const apply = async (amount: number) => {
     // ]);
     // console.log(applicant.address, "upload authorization");
 
-    await submitEvidence(
-      ["0xe81830c7d3b35e34354d0805498657ef06efa0980bb1e4a9b66197dfdd0883c9"],
-      applicant
+    const randomIndex = Math.floor(
+      Math.random() * UPLOADED_EVIDENCE_HASHES.length
     );
+    const randomHash = UPLOADED_EVIDENCE_HASHES[randomIndex];
+    await submitEvidence([randomHash], applicant);
+
     console.log(applicant.address, "evidence provided");
   };
 
