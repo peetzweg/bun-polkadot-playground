4226b7af5e13c500e06f425055480a8236ca557c
peetzweg/ <phil.czek@gmail.com>
2024-08-09 16:59:47 +0200

    defines and tests runtime api calls

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,10 +1,11 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import "../interfaces/people/augment-api";
-import "../interfaces/people/augment-types";
 import "../interfaces/bulletin/augment-api";
 import "../interfaces/bulletin/augment-types";
+import "../interfaces/people/augment-api";
+import "../interfaces/people/augment-types";
 import "../interfaces/rococo/augment-api";
 import "../interfaces/rococo/augment-types";
+import { runtime } from "./runtimeApi";
 
 type ChainName = "People" | "Bulletin" | "Relay";
 
@@ -24,9 +25,21 @@ export const getApi = async (chain: ChainName): Promise<ApiPromise> => {
   if (apis[chain] !== undefined) {
     return apis[chain] as ApiPromise;
   }
+
+  const types = {
+    Entropy: "[u8;32]",
+    EntropyVec: "Vec<Entropy>",
+    Member: "[u8;33]",
+    MembersVec: "Vec<Member>",
+    Proof: "[u8;788]",
+    Alias: "[u8;32]",
+  };
+
   const wsProvider = new WsProvider(`wss://${rpcs[chain]}`);
   const api = await ApiPromise.create({
     provider: wsProvider,
+    runtime,
+    types,
   });
   apis[chain] = api;
   return api;
