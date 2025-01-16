8cce3992953ed8195b5c8eaa70970c5ac0eb9d56
peetzweg/ <phil.czek@gmail.com>
2025-01-16 13:01:31 +0100

    first working version of the rework of the apply script

--- src/features/apply.ts
+++ src/features/apply.ts
@@ -1,95 +1,158 @@
-import { getApi } from "../apis";
+import { getTypedApi } from "../apis";
 
-import { Keyring } from "@polkadot/keyring";
-import { KeyringPair } from "@polkadot/keyring/types";
-import { mnemonicGenerate } from "@polkadot/util-crypto";
-import { resolveOnInBlock } from "../utils/resolveOn";
-import { resolveOnSome } from "../utils/resolveOnSome";
-import { PalletProofOfInkCandidate } from "@polkadot/types/lookup";
-import { PHOTO_EVIDENCE_HASHES, VIDEO_EVIDENCE_HASHES } from "./evidence";
+import { generateMnemonic } from "@polkadot-labs/hdkd-helpers";
+
+import {
+  AccountId,
+  Enum,
+  PolkadotSigner,
+  SS58String,
+  Transaction,
+  TxFinalizedPayload,
+} from "polkadot-api";
+import {
+  Applicant,
+  getDevPolkadotSigner,
+  mnemonicToApplicant,
+} from "../keyring";
+import { storeMnemonic } from "./new";
+import { MultiAddress, people } from "@polkadot-api/descriptors";
+import { one_shot } from "../verifiable/verifiable";
+
+async function signAndSubmit<
+  Pallet extends string,
+  Name extends string,
+  Asset,
+  Args extends {} | undefined,
+  T extends Transaction<Args, Pallet, Name, Asset>
+>(tx: T, signer: PolkadotSigner) {
+  return new Promise((resolve, reject) => {
+    tx.signSubmitAndWatch(signer, {
+      customSignedExtensions: {
+        VerifyMultiSignature: { value: Enum("Disabled") },
+        AsPerson: { value: undefined },
+        ProvideForReferral: { value: undefined },
+      },
+    } as any).subscribe({
+      next: (e) => {
+        // console.log(e.type);
+
+        if (e.type === "finalized") {
+          console.log(
+            `-> https://dev.papi.how/explorer/${e.block.hash}#networkId=custom&endpoint=wss%3A%2F%2Fpop-testnet.parity-lab.parity.io%3A443%2F9910`
+          );
+          resolve(e);
+        }
+      },
+      error: (e) => {
+        console.error(e);
+        reject(e);
+      },
+      complete() {
+        console.log("Completed!");
+      },
+    });
+  });
+}
 
 export const apply = async (amount: number) => {
-  const People = await getApi("People");
+  const People = await getTypedApi("People");
 
-  const keyring = new Keyring({ type: "sr25519", ss58Format: 0 });
-  const alice = keyring.createFromUri("//Alice");
+  const logFunds = async (name: string, address: SS58String) => {
+    const account = await People.query.System.Account.getValue(address);
+    console.log(` ${address} has ${account.data.free} ${name}`);
+    return account.data;
+  };
 
-  const decimals = People.registry.chainDecimals[0];
+  const alice = getDevPolkadotSigner("//Alice");
 
-  const [apply, commit, submitEvidence] = [
-    People.tx.proofOfInk.apply,
-    People.tx.proofOfInk.commit,
-    People.tx.proofOfInk.submitEvidence,
-  ].map((fn) => resolveOnInBlock(fn));
+  const graduateAccount = async (applicant: Applicant) => {
+    console.log("applying");
+    try {
+      await signAndSubmit(People.tx.ProofOfInk.apply(), applicant.signer);
 
-  const graduateAccount = async (applicant: KeyringPair) => {
-    await apply([], applicant);
-    console.log(applicant.address, "applied");
-
-    await commit([{ ProceduralAccount: 8 }, null], applicant);
-    console.log(applicant.address, "committed");
-
-    const candidacyOption = await People.query.proofOfInk.candidates(
-      applicant.address
-    );
-    if (candidacyOption.isNone)
-      throw new Error(`No candidacy found for '${applicant.address}'`);
-
-    const candidacy: PalletProofOfInkCandidate = candidacyOption.unwrap();
-    if (!candidacy.isSelected)
-      throw new Error(`Candidacy for '${applicant.address}' is not selected`);
-
-    console.log(candidacy.asSelected.toHuman());
-
-    if (candidacy.asSelected.allocation.isInitial) {
-      const randomIndex = Math.floor(
-        Math.random() * PHOTO_EVIDENCE_HASHES.length
-      );
-      const randomHash = PHOTO_EVIDENCE_HASHES[randomIndex];
-      await submitEvidence([randomHash], applicant);
-      console.log(applicant.address, "photo evidence provided");
-    } else if (candidacy.asSelected.allocation.isFull) {
-      const randomIndex = Math.floor(
-        Math.random() * VIDEO_EVIDENCE_HASHES.length
-      );
-      const randomHash = VIDEO_EVIDENCE_HASHES[randomIndex];
-      await submitEvidence([randomHash], applicant);
-      console.log(applicant.address, "video evidence provided");
+      console.log(applicant.address, "applied");
+    } catch (e) {
+      console.dir(e);
     }
-  };
 
-  const prepareAccounts = (mnemonics: string[]) => {
-    return mnemonics.map((mnemonic) => {
-      const applicant = keyring.createFromUri(mnemonic);
-      const ss58Address = People.createType("AccountId", applicant.address);
-      console.log("new Account", ss58Address.toString());
-      Bun.write(`./accounts/${ss58Address.toHuman()}`, mnemonic);
-      return applicant;
+    const commitTx = People.tx.ProofOfInk.commit({
+      choice: Enum("ProceduralAccount", 8),
+      require_id: undefined,
     });
+    await signAndSubmit(commitTx, applicant.signer);
+    // await commit([{ ProceduralAccount: 8 }, null], applicant);
+    // console.log(applicant.address, "committed");
+
+    // const candidacyOption = await People.query.proofOfInk.candidates(
+    //   applicant.address
+    // );
+    // if (candidacyOption.isNone)
+    //   throw new Error(`No candidacy found for '${applicant.address}'`);
+
+    // const candidacy: PalletProofOfInkCandidate = candidacyOption.unwrap();
+    // if (!candidacy.isSelected)
+    //   throw new Error(`Candidacy for '${applicant.address}' is not selected`);
+
+    // console.log(candidacy.asSelected.toHuman());
+
+    // if (candidacy.asSelected.allocation.isInitial) {
+    //   const randomIndex = Math.floor(
+    //     Math.random() * PHOTO_EVIDENCE_HASHES.length
+    //   );
+    //   const randomHash = PHOTO_EVIDENCE_HASHES[randomIndex];
+    //   await submitEvidence([randomHash], applicant);
+    //   console.log(applicant.address, "photo evidence provided");
+    // } else if (candidacy.asSelected.allocation.isFull) {
+    //   const randomIndex = Math.floor(
+    //     Math.random() * VIDEO_EVIDENCE_HASHES.length
+    //   );
+    //   const randomHash = VIDEO_EVIDENCE_HASHES[randomIndex];
+    //   await submitEvidence([randomHash], applicant);
+    //   console.log(applicant.address, "video evidence provided");
+    // }
   };
 
-  const fundAccounts = async (addresses: string[]) => {
-    const fundCalls = addresses.map((address) =>
-      People.tx.balances.transferKeepAlive(
-        address,
-        Number(0.0105) * 10 ** decimals
-      )
+  const fundAccounts = async (applicants: Applicant[]) => {
+    const transfers = applicants.map((applicant) =>
+      People.tx.Balances.transfer_allow_death({
+        dest: MultiAddress.Id(applicant.address),
+        value: 100_000_000_000n,
+      })
     );
 
-    return resolveOnInBlock(People.tx.utility.batch)([fundCalls], alice);
+    const batchTx = People.tx.Utility.batch({
+      calls: transfers.map((t) => t.decodedCall),
+    });
+
+    await signAndSubmit(batchTx, alice.signer);
   };
 
-  const mnemonics = [...Array(Number(amount)).keys()].map(() =>
-    mnemonicGenerate(24)
+  const mnemonics = [...Array(Number(amount)).keys()].map(
+    () => generateMnemonic(24 * 8) // 24 words
   );
-  const applicants = prepareAccounts(mnemonics);
 
-  await fundAccounts(applicants.map((a) => a.address));
+  const applicants = mnemonics
+    .map((m) => storeMnemonic(m))
+    .map((m) => mnemonicToApplicant(m));
+
+  await fundAccounts(applicants);
   console.log("All accounts funded");
+
+  for await (const applicant of applicants) {
+    await logFunds("Applicant:", applicant.address);
+  }
 
   for await (const applicant of applicants) {
     await graduateAccount(applicant);
   }
 
-  console.log("All accounts opened a mob rule case");
+  // console.log("All accounts opened a mob rule case");
+};
+
+const debugOutcome = (outcome: TxFinalizedPayload) => {
+  console.log(outcome.events.map((e) => `${e.type}.${e.value.type}`));
+  console.log(
+    `https://dev.papi.how/explorer/${outcome.block.hash}#networkId=custom&endpoint=wss%3A%2F%2Fpop-testnet.parity-lab.parity.io%3A443%2F9910`
+  );
 };
--- src/features/new.ts
+++ src/features/new.ts
@@ -1,18 +1,22 @@
-import { generateMnemonic, ss58Encode } from "@polkadot-labs/hdkd-helpers";
+import { generateMnemonic } from "@polkadot-labs/hdkd-helpers";
 
+import { AccountId } from "polkadot-api";
 import { mnemonicToKeyPair } from "../keyring";
+
+export const storeMnemonic = (mnemonic: string) => {
+  const { keyPair } = mnemonicToKeyPair(mnemonic);
+
+  const ss58Address = AccountId(42, 32).dec(keyPair.publicKey);
+
+  console.log("new Account", ss58Address);
+  Bun.write(`./accounts/${ss58Address}`, mnemonic);
+  return mnemonic;
+};
 
 export const newAccounts = async (amount: number) => {
   [...Array(Number(amount)).keys()]
     .map(() => generateMnemonic(24 * 8))
-    .map((mnemonic) => {
-      const { keyPair } = mnemonicToKeyPair(mnemonic);
-
-      const ss58Address = ss58Encode(keyPair.publicKey, 0);
-
-      console.log("new Account", ss58Address);
-      Bun.write(`./accounts/${ss58Address}`, mnemonic);
-    });
+    .map((mnemonic) => storeMnemonic(mnemonic));
 
   console.info(`Created ${amount} new accounts in ./accounts`);
 };
